f449e12f836ff8c687a7333a34c86d033ab75046
vcccv <2742927845@qq.com>
2023-11-24 18:13:16 +0800

    回退版本

--- file/编译.bat
+++ file/编译.bat
@@ -1,2 +1,2 @@
 ECHO ON
-START /wait  jasshelper\jasshelper.exe --scriptonly jass\24\common.j  jass\24\blizzard.j Vjass\war3map.j war3map.j
+START /wait  jasshelper\jasshelper.exe --scriptonly jass\24\common.j  jass\24\blizzard.j Scripts\war3map.j war3map.j
